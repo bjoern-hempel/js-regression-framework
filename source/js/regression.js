6dd83e1c1ecc2dc7084fd839094a97c3c8065951
Björn Hempel <bjoern@hempel.li>
2018-08-27 23:39:41 +0200

    Update regression.js

--- source/js/regression.js
+++ source/js/regression.js
@@ -2,8 +2,15 @@
  * A class to analyse data structures (regression)
  *
  * @author  Björn Hempel <bjoern@hempel.li>
- * @version 1.0 (2018-08-22)
+ * @version 1.0 (2018-08-27)
  */
 class Regression {
 
+    /**
+     * The constructor of the Base class.
+     *
+     */
+    constructor() {
+        this.name = 'Regression';
+    }
 }
